ea62615fb28711d02f3667d1140b10556c97150d
eduardoQuea <eduardoquea3@gmail.com>
2025-05-16 13:17:23 -0500

    feat: add button component and page initial

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,9 +11,16 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as ControlsImport } from './routes/controls'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const ControlsRoute = ControlsImport.update({
+  id: '/controls',
+  path: '/controls',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   id: '/',
@@ -32,6 +39,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/controls': {
+      id: '/controls'
+      path: '/controls'
+      fullPath: '/controls'
+      preLoaderRoute: typeof ControlsImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +53,37 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/controls': typeof ControlsRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/controls': typeof ControlsRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/controls': typeof ControlsRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/controls'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/controls'
+  id: '__root__' | '/' | '/controls'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  ControlsRoute: typeof ControlsRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  ControlsRoute: ControlsRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +96,15 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/controls"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/controls": {
+      "filePath": "controls.tsx"
     }
   }
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,35 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { createFileRoute, Link } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div className="min-h-screen w-full">NONE</div>
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center gap-4 p-4">
+      <div className="grid grid-cols-3 items-center gap-4">
+        <h2 className="text-4xl font-bold col-span-3 text-center">Base Components</h2>
+        <Link
+          to="/controls"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          Controls
+        </Link>
+        <Link
+          to="/controls"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          React Hook Form
+        </Link>
+        <Link
+          to="/controls"
+          className={cn(buttonVariants({ variant: "outline" }))}
+        >
+          TanStack Form
+        </Link>
+      </div>
+    </div>
+  )
 }
